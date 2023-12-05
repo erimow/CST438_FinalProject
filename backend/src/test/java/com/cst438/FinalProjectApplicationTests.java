f08cb322e45aababfd0da3d90beba658703f48b1
Erik <ermorris@csumb.edu>
2023-12-04 23:00:16 -0800

    Fixed some small frontend issues, and added jUnitTests for my CRUD functions

--- backend/src/test/java/com/cst438/FinalProjectApplicationTests.java
+++ backend/src/test/java/com/cst438/FinalProjectApplicationTests.java
@@ -1,13 +1,79 @@
 package com.cst438;
 
+import com.cst438.controllers.MediaController;
+import com.cst438.domain.Media;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FinalProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void getAllMedia_shouldReturnAllMedia() throws Exception {
+        mockMvc.perform(get("/api/media"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+    }
+
+    @Test
+    public void createMedia_shouldCreateNewMedia() throws Exception {
+        Media newMedia = new Media(); // Create a new Media object 
+
+        mockMvc.perform(post("/api/media")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(newMedia)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+    }
+
+    @Test
+    public void getMediaById_shouldReturnCorrectMedia() throws Exception {
+        int mediaId = 1; 
+
+        mockMvc.perform(get("/api/media/{id}", mediaId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+       
+    }
+
+    @Test
+    public void updateMedia_shouldUpdateExistingMedia() throws Exception {
+        int mediaId = 1; 
+        Media updatedMedia = new Media(); 
+        
+        mockMvc.perform(put("/api/media/{id}", mediaId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedMedia)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+        
+    }
+
+    @Test
+    public void deleteMedia_shouldDeleteMedia() throws Exception {
+        int mediaId = 1; 
+
+        mockMvc.perform(delete("/api/media/{id}", mediaId))
+                .andExpect(status().isOk());
+      
+    }
 
 }
--- frontend/src/components/AddReview.js
+++ frontend/src/components/AddReview.js
@@ -52,18 +52,18 @@ function AddReview(props) {
       <div>
         <h3>Add Review</h3>
         <h4 id="amessage" >{message}&nbsp;</h4>
-        <h5>Media Image</h5>
-        <input name="picURL" value={factors.picURL || ""} type="text" onChange={(e) => handleChange(e)}/>
+        <h5>Media Image (URL)</h5>
+        <input name="picURL" style={{width: '500px'}} value={factors.picURL || ""} type="text" onChange={(e) => handleChange(e)}/>
         <hr/><h5>Media Title</h5>
         <input name="title" value={factors.title || ""} type="text" onChange={(e) => handleChange(e)}/>
         <hr/> <h5>Media Type</h5>
         <input name='type' value={(factors.type)? factors.type:""} type="text" onChange={(e) => handleChange(e)}/>
         <hr/><h5>Genre</h5>
         <input name='genre' value={(factors.genre)? factors.genre:""} type='text' onChange={(e) => handleChange(e)}/>
-        <hr/><h5>Rating</h5>
+        <hr/><h5>Rating (1-10)</h5>
         <input name='rating' value={(factors.rating)? factors.rating:""} min="1"max="10"type='number' onChange={(e) => handleChange(e)}/>
         <hr/><h5>Written Review</h5>
-        <input name='review' value={(factors.review)? factors.review:""} type="text" onChange={(e) => handleChange(e)}/> <hr></hr>
+        <input name='review' style={{width: '500px', wordWrap: 'break-word', height: '30px'}} value={(factors.review)? factors.review:""} type="text" onChange={(e) => handleChange(e)}/> <hr></hr>
         <button id='sassign' type="button" margin="auto" onClick={saveMedia}>Save Review</button> <br></br>
         <button><Link to={`/`}>Back</Link></button>
       </div>
